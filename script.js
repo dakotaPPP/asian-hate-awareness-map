e9650e8e56be88cc4f2401a7a56b8abf4a80c769
Dakota Pound <76417971+dakotaPPP@users.noreply.github.com>
2021-10-01 03:56:50 +0000

    made about page real

--- script.js
+++ script.js
@@ -1,36 +1,28 @@
 var about = document.getElementById("menu-about");
 var info = document.getElementById("menu-more-info");
 var moreInfo = document.getElementById("menu-more-info");
-var aboutAreaBody = document.getElementById("about-page-body");
-var aboutAreaHeader = document.getElementById("about-page-header");
-var infoAreaBody = document.getElementById("info-page-body");
-var infoAreaHeader = document.getElementById("info-page-header");
+var aboutArea = document.getElementById("about");
+var infoArea = document.getElementById("info");
 const closeButton = document.querySelectorAll(".x");
 
 /* event listner for the "ABOUT" button */
 about.onclick = function(){
-  aboutAreaBody.style.visibility = "visible";
-  aboutAreaHeader.style.visibility = "visible";
+  aboutArea.style.visibility = "visible";
 }
 
 /* event listener for the "MORE INFORMATION"button */
 info.onclick = function(){
-  infoAreaBody.style.visibility = "visible";
-  infoAreaHeader.style.visibility = "visible";
+  infoArea.style.visibility = "visible";
 }
 
 /* event listener for the buttons of class "x" */
 closeButton.forEach(el => el.addEventListener("click", event => {
-  aboutAreaBody.style.visibility = "hidden";
-  aboutAreaHeader.style.visibility = "hidden";
-  infoAreaBody.style.visibility = "hidden";
-  infoAreaHeader.style.visibility = "hidden";
+  aboutArea.style.visibility = "hidden";
+  infoArea.style.visibility = "hidden";
 }));
 
 /* event listener for whenever the user clicks the map */
 map.on('click', (e) => {
-  aboutAreaBody.style.visibility = "hidden";
-  aboutAreaHeader.style.visibility = "hidden";
-  infoAreaBody.style.visibility = "hidden";
-  infoAreaHeader.style.visibility = "hidden";
+  aboutArea.style.visibility = "hidden";
+  infoArea.style.visibility = "hidden";
 });
