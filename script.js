fc1530817589285e343b05028caf23fea06ccf02
Dakota Pound <76417971+dakotaPPP@users.noreply.github.com>
2021-10-02 04:37:35 +0000

    added settings animation, made about page and more information page more real

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 var about = document.getElementById("menu-about");
 var info = document.getElementById("menu-more-info");
-var moreInfo = document.getElementById("menu-more-info");
+var settings = document.getElementById("settings-area")
 var aboutArea = document.getElementById("about");
 var infoArea = document.getElementById("info");
+var settingArea = document.getElementById("settings");
 const closeButton = document.querySelectorAll(".x");
+var settingsOpened = false;
 
 /* event listner for the "ABOUT" button */
-about.onclick = function(){
-  aboutArea.style.visibility = "visible";
-}
+about.onclick = function(){ aboutArea.style.visibility = "visible";}
 
 /* event listener for the "MORE INFORMATION"button */
-info.onclick = function(){
-  infoArea.style.visibility = "visible";
-}
+info.onclick = function(){ infoArea.style.visibility = "visible";}
 
+settings.onclick = function(){
+  settingsToggle();
+}
 /* event listener for the buttons of class "x" */
 closeButton.forEach(el => el.addEventListener("click", event => {
   aboutArea.style.visibility = "hidden";
@@ -25,4 +26,12 @@ closeButton.forEach(el => el.addEventListener("click", event => {
 map.on('click', (e) => {
   aboutArea.style.visibility = "hidden";
   infoArea.style.visibility = "hidden";
+  if (settingsOpened){ settingsToggle();}
 });
+
+function settingsToggle(){
+  settingArea.style.visibility = "visible";
+  $('.settings-close').toggleClass('settings-open');
+  $('.borderOn').toggleClass('borderOff');
+  settingsOpened = !settingsOpened;
+}
