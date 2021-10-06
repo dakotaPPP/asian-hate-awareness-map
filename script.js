946f715aeab5b4fad8511f135be74ee47991d24e
Dakota Pound <76417971+dakotaPPP@users.noreply.github.com>
2021-10-06 17:38:46 +0000

    heatmap mode now toggles properly

--- script.js
+++ script.js
@@ -5,22 +5,36 @@ var aboutArea = document.getElementById("about");
 var infoArea = document.getElementById("info");
 var settingArea = document.getElementById("settings");
 const closeButton = document.querySelectorAll(".x");
+const heatmapMode = document.querySelectorAll(".setting1-checkbox");
 var settingsOpened = false;
+var heatmapModeBool = false;
 
 /* event listner for the "ABOUT" button */
 about.onclick = function(){ aboutArea.style.visibility = "visible";}
 
-/* event listener for the "MORE INFORMATION"button */
+/* event listener for the "MORE INFORMATION" button */
 info.onclick = function(){ infoArea.style.visibility = "visible";}
 
-settings.onclick = function(){
-  settingsToggle();
-}
+settings.onclick = function(){ settingsToggle(); }
+
 /* event listener for the buttons of class "x" */
 closeButton.forEach(el => el.addEventListener("click", event => {
   aboutArea.style.visibility = "hidden";
   infoArea.style.visibility = "hidden";
 }));
+
+/* event listener for the heatmap button */
+$('input[type="checkbox"]').change(function() {
+    if(heatmapModeBool){
+      map.setStyle("mapbox://styles/larscchristensen/ckud120ny3p7117pvruvsnuk5");
+      heatmapModeBool = !heatmapModeBool;
+    } else {
+      map.setStyle("mapbox://styles/larscchristensen/cktj10n763sgi18rvyyh2e5rm");
+      heatmapModeBool = !heatmapModeBool;
+    }
+    $('.marker-shown').toggleClass('marker-hidden'); 
+});
+
 
 /* event listener for whenever the user clicks the map */
 map.on('click', (e) => {
@@ -41,21 +55,6 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-//this var sets the zoom multiplier on the map to make it fit different sized monitors 
-var zoomMultiplier = 2 + ($(document).width()/850);
-if ($(document).width()<=600){
-  zoomMultiplier =  1 + ($(document).width()/400);
-} else if ($(document).width()>=2000) {
-  zoomMultiplier =  2.6 + ($(document).width()/1200);
-}
-mapboxgl.accessToken = 'pk.eyJ1IjoibGFyc2NjaHJpc3RlbnNlbiIsImEiOiJja3RqMHd6bHAxN2J0Mnd1ZTBhMnFqeDBvIn0.myfePg-ekbueQ6nAKyVRog'; //access token for map
-const map = new mapboxgl.Map({
-  container: 'map', // container ID
-  style: 'mapbox://styles/larscchristensen/cktj10n763sgi18rvyyh2e5rm/draft', // style URL
-  enter: [-95.75, 38.5119], // starting position [lng, lat]
-  zoom: zoomMultiplier // starting zoom
-});
-
 /* pulls information from data.csv and creates a marker and popup icon at the coordinates and information for the article */
 fetch('testdata.csv')
   .then(response => response.text())
@@ -64,7 +63,7 @@ fetch('testdata.csv')
       for(var i = 1; i<(csvArr.data.length); i++){
         // create a HTML element for each feature
         const el = document.createElement('div');
-        el.className = 'marker';
+        el.className = 'marker marker-shown';
         // make a marker for each feature and add it to the map
         new mapboxgl.Marker(el)
         //the randomInt is so when data points come from the same city the points don't just overlap
